e11abaaf2c5460e8305bc2fefd1dabb5bddb3bf9
Azriel Attaufiqi <azrielrisywan@gmail.com>
2024-01-29 17:10:13 +0700

    Add color customization to QR code generation

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -11,9 +11,20 @@ class QrCodeController extends Controller
     {
         $qr = new QrCode();
 
+        $redcolorQr = $request->redcolorQr ?? 0;
+        $greencolorQr = $request->greencolorQr ?? 0;
+        $bluecolorQr = $request->bluecolorQr ?? 0;
+
+        $redbgcolorQr = $request->redbgcolorQr ?? 255;
+        $greenbgcolorQr = $request->greenbgcolorQr ?? 255;
+        $bluebgcolorQr = $request->bluebgcolorQr ?? 255;
+        
         $stringQr = $request->stringQr;
 
-        return $qr::size(200)->generate($stringQr);
+        return $qr::size(200)
+                    ->color($redcolorQr, $greencolorQr, $bluecolorQr)
+                    ->backgroundColor($redbgcolorQr, $greenbgcolorQr, $bluebgcolorQr)
+                    ->generate($stringQr);
 
     }
 }
